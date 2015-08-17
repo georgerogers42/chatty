4c259184413c4289ba522256b2a21c3afcdf231c
George Rogers <grogers385@gmail.com>
2015-08-17 13:30:56 -0500

    true

--- lib/chatty/web/await.ex
+++ lib/chatty/web/await.ex
@@ -1,9 +1,9 @@
 defmodule Chatty.Web.Await do
   def init(_kind, req, state) do
-    {:ok, msgs} = Chatty.Chat.Reactor.await
-    {:ok, req, msgs}
+    {:ok, req, state}
   end
   def handle(req, state) do
+    {:ok, _msgs} = Chatty.Chat.Reactor.await
     {:ok, req} = :cowboy_req.reply(200, [{"content-type", "application/json"}], [Poison.encode!(%{ready: true}), "\n"], req)
     {:ok, req, state}
   end
