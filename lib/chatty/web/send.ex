a5e1d390c56ac898859b5af1408756f73a63d34e
George Rogers <grogers385@gmail.com>
2015-08-18 13:14:20 -0500

    update

--- lib/chatty/web/send.ex
+++ lib/chatty/web/send.ex
@@ -1,4 +1,5 @@
 defmodule Chatty.Web.Send do
+  use Timex
   def init(_kind, req, state) do
     {"POST", req} = :cowboy_req.method(req)
     {:ok, req, state}
@@ -6,7 +7,7 @@ defmodule Chatty.Web.Send do
   def handle(req, state) do
     {:ok, vals, req} = :cowboy_req.body_qs(req)
     vals = vals |> Enum.into(HashDict.new)
-    :ok = Chatty.Chat.Reactor.put([vals["user"], vals["msg"]])
+    :ok = Chatty.Chat.Reactor.put([Date.now |> DateFormat.format!("{ISOz}"), vals["user"], vals["msg"], ])
     {:ok, req} = :cowboy_req.reply(302, [{"location", "/"}], "", req)
     {:ok, req, state}
   end
