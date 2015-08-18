a31163e6703244f1ee897821ca99e8dd2f054cb3
George Rogers <grogers385@gmail.com>
2015-08-18 09:51:56 -0500

    update

--- mix.exs
+++ mix.exs
@@ -36,7 +36,6 @@ defmodule Chatty.Mixfile do
       {:cowboy, "~> 1.0.0"},
       {:poison, ">= 0.0.0"},
       {:timex, ">= 0.0.0"},
-      {:chan, ">= 0.0.2", git: "https://bitbucket.org/georgerogers42/chan.git"},
     ]
   end
 end
