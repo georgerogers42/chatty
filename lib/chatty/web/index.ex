a61ad4f5aaf39caa15f3e7c90f3f11d53bcfddd5
George Rogers <grogers385@gmail.com>
2015-08-22 18:35:48 -0500

    update

--- lib/chatty/web/index.ex
+++ lib/chatty/web/index.ex
@@ -3,7 +3,8 @@ defmodule Chatty.Web.Index do
   def init(_kind, req, state) do
     {"GET", req} = :cowboy_req.method(req)
     {room, req} = :cowboy_req.binding(:room, req)
-    {{ip, _port}, req} = :cowboy_req.peer(req)
+    {{{a,b,c,d}, _port}, req} = :cowboy_req.peer(req)
+    ip = :io_lib.format("~b.~b.~b.~b", [a, b, c, d])
     state = Enum.into [{:room, room}, {:ip, ip}], state
     {:ok, req, state}
   end
